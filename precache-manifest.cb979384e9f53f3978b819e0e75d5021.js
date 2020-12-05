67c6a491f315706f6c81a912876cf08dc4ed8f2b
Thanuskanth <thanus1207.tt@gmail.com>
2020-12-05 22:00:36 +0530

    Updates

---
+++ precache-manifest.cb979384e9f53f3978b819e0e75d5021.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f71b5275a634d1719db46108637cb1",
+    "url": "/Sample/index.html"
+  },
+  {
+    "revision": "e4d5113b7013ee250000",
+    "url": "/Sample/static/css/2.bb7a1233.chunk.css"
+  },
+  {
+    "revision": "9a96e66b18d57085def3",
+    "url": "/Sample/static/css/main.c7082ba4.chunk.css"
+  },
+  {
+    "revision": "e4d5113b7013ee250000",
+    "url": "/Sample/static/js/2.befd0254.chunk.js"
+  },
+  {
+    "revision": "2ad866cdf702a9732740901cbbe44edf",
+    "url": "/Sample/static/js/2.befd0254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a96e66b18d57085def3",
+    "url": "/Sample/static/js/main.2b06257e.chunk.js"
+  },
+  {
+    "revision": "23f353dff1031296e0b4",
+    "url": "/Sample/static/js/runtime-main.999fd48f.js"
+  },
+  {
+    "revision": "b09871f1049f969b81296845a6c6b8f3",
+    "url": "/Sample/static/media/Debit Note Print.b09871f1.jpg"
+  },
+  {
+    "revision": "884835ea82af7f879855f6576dd50e92",
+    "url": "/Sample/static/media/Invoice Print 01.884835ea.jpg"
+  },
+  {
+    "revision": "a6162ea63cdaf258184aa3c9631d1289",
+    "url": "/Sample/static/media/Invoice Print 02.a6162ea6.jpg"
+  },
+  {
+    "revision": "b93744d3767cc21c84323c4429ff764c",
+    "url": "/Sample/static/media/Receipt Print.b93744d3.jpg"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/Sample/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/Sample/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/Sample/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/Sample/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/Sample/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/Sample/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/Sample/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/Sample/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/Sample/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/Sample/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/Sample/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/Sample/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/Sample/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/Sample/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/Sample/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "123fe72000ede19535224174850649ec",
+    "url": "/Sample/static/media/logo.123fe720.PNG"
+  }
+]);
